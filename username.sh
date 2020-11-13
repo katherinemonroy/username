c937583a2a78faabf066a537c0bf16a0fe9fc40c
katherinemonroy <kmonroy@chapman.edu>
2020-11-13 10:57:10 -0800

    fixes code i think

--- username.sh
+++ username.sh
@@ -3,10 +3,10 @@
 # katherinemonroy
 echo "Enter a username: "
 read NAME
-while echo "$NAME" | grep -E -v "^[a-z][0-9]{5}$" > /dev/null 2>&1
+while echo "$NAME" | grep -E -v "^[a-z][[[a-z0-9_]{2,11}$" > /dev/null 2>&1
 do
-	echo "You must enter a valid ZIP code - five digits only!"
-	echo "Enter a five-digit ZIP code: "
+	echo "You must use lowercase letters,digits, and an aunderscore character. Remember to start with a lowercase letter!"
+	echo "Enter a username: "
 	read -r NAME
 done
 echo "Thank you"
